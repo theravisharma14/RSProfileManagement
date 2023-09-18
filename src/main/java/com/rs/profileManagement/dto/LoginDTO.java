ab379e52c93e41b620129e03df87b8854f4529a7
ravi sharma <ravisharma7414@gmail.com>
2023-09-18 16:05:45 +0530

    test

---
+++ src/main/java/com/rs/profileManagement/dto/LoginDTO.java
@@ -0,0 +1,21 @@
+package com.rs.profileManagement.dto;
+
+public class LoginDTO {
+	private String userEmail;
+	private String userPass;
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPass() {
+		return userPass;
+	}
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+	
+	
+}
