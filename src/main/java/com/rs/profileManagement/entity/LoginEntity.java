1716639509d69a3d3ac00411ff4823c640df8ea1
ravi sharma <ravisharma7414@gmail.com>
2023-09-18 17:28:02 +0530

    updateDATE

--- src/main/java/com/rs/profileManagement/entity/LoginEntity.java
+++ src/main/java/com/rs/profileManagement/entity/LoginEntity.java
@@ -10,8 +10,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name= "RS_PROFILE_MANAGEMENT")
 public class LoginEntity {
 
 	@Id
